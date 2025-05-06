6675cea9ab932cf619bb22257ef582b03a867a7a
Anindita Rahma <aninditarhmz@gmail.com>
2025-05-06 17:13:09 +0700

    nambahin back button di halaman play

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,12 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public void PlayGame()
+    public void ChooseGame()
     {
         SceneManager.LoadScene("Play");
     }
+
     public void ExitToExitScene()
     {
         SceneManager.LoadScene("Exit");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Game1()
+    {
+        SceneManager.LoadScene("Game1");
+    }
+
+    public void Game2()
+    {
+        SceneManager.LoadScene("Game2");
+    }
+
+    public void Game3()
+    {
+        SceneManager.LoadScene("Game3");
     }
 }
